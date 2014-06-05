10421eb842971f6bc64667bd41338e8cfeb03807
qiminl <qiminl@sfu.ca>
2014-06-05 14:42:00 -0700

    a few scenes added as a test, the UI things can be replaced later

---
+++ AntLion/Assets/Scripts/playButtonAction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playButtonAction : MonoBehaviour {
+
+	public Object button;
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		if (GUI.Button (new Rect (550, 250, 50, 50), this.guiTexture.texture))
+			Debug.Log("Clicked the button with an image");
+	}
+}
---
+++ AntLion/Assets/Scripts/textScrolling.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textScrolling : MonoBehaviour {
+
+	float charactersPerSecond= 8.0f;
+	string text = "Here is some text to scroll";
+	Rect rect;
+	
+	private string textUsing;
+	private string scrollBasis;
+	private string scrollText;
+	
+	private int currChar = 0;
+	private float timer = 0.0f;
+	
+	void Start () {
+		this.guiText.text = "If Ant lion guess your movement right,\n" +
+			"you will get caught \n" +
+			"and be dragged down for two blocks if you choose to move vertically\n" +
+			"or be dragged down for one block if you move horizontally\n" +
+			"climb to the top of the screen to get rid of the ant lion\n" +
+			"so be careful and have fun!\n";
+	}
+	/*
+	void Update () {
+		if (textUsing != text)
+			NewText();
+		
+		float secondsPerCharacter  = 1.0f / charactersPerSecond;
+		if (timer > secondsPerCharacter) {
+			int iT  = Mathf.FloorToInt(timer / secondsPerCharacter);
+			currChar = (currChar + iT) % textUsing.Length;
+			timer -= (float)iT * secondsPerCharacter;
+			scrollText = scrollBasis.Substring(currChar, textUsing.Length);  
+		}
+		timer += (float)Time.deltaTime;
+		this.guiText.text = textUsing;
+	}
+	
+	void OnGUI() {
+		GUI.Label(rect, scrollText);
+	}
+	
+	void NewText() {
+		textUsing = text;
+		scrollBasis = textUsing+textUsing;
+		currChar = 0;
+		scrollText = scrollBasis.Substring(currChar, textUsing.Length);
+		timer = 0.0f;
+	}*/
+}
+
